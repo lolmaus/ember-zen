9eadf2d402670c0503cdef8d0e6b6d728a434324
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2017-06-18 16:15:01 +0300

    ESLint config

--- testem.js
+++ testem.js
@@ -1,12 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+  "test_page"        : "tests/index.html?hidepassed",
+  "disable_watching" : true,
+  "launch_in_ci"     : [
+    "Chrome",
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
+  "launch_in_dev" : [
+    "Chrome",
   ]
-};
+}
